5477e27d3eebc598652d2d2e15bd3d14ebd24209
satokodesu <satou-gcv@yamanashi-ken.ac.jp>
2022-03-08 13:56:26 +0900

    [add]記事詳細画面

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,6 +15,16 @@ class Postcontroller extends Controller
  */
 public function index(Post $post)
     {
-        return view('posts/index')->with(['posts' => $post->get()]);
+        return view('posts/index')->with(['posts' => $post->getPaginateByLimit(1)]);
+    }
+    /**
+ * 特定IDのpostを表示する
+ *
+ * @params Object Post // 引数の$postはid=1のPostインスタンス
+ * @return Reposnse post view
+ */
+public function show(Post $post)
+    {
+        return view('posts/show')->with(['post' => $post]);
     }
 }
